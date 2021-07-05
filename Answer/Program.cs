325c2ba2c7e369108c8ccc7cca52e377e231ead2
suzuryg <suzuryg@outlook.com>
2021-07-05 23:05:58 +0900

    add: 標準入出力のテンプレートを追加
    https://qiita.com/naminodarie/items/dce121a992cbdca69a78

--- Answer/Program.cs
+++ Answer/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Kzrnm.Competitive.IO;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
-namespace Answer
+namespace AtCoder.Answer
 {
-    class Program
+    public partial class Program
     {
-        static void Main(string[] args)
+        static void Main() => new Program(new ConsoleReader(), new ConsoleWriter()).Run();
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)] public ConsoleReader cr;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)] public ConsoleWriter cw;
+        public Program(ConsoleReader r, ConsoleWriter w)
         {
-            Console.WriteLine("Hello World!");
+            this.cr = r;
+            this.cw = w;
+            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+        }
+    }
+    public partial class Program
+    {
+        public void Run()
+        {
+            SourceExpander.Expander.Expand();
+            var res = Calc();
+            if (res is double) cw.WriteLine(((double)res).ToString("0.####################", System.Globalization.CultureInfo.InvariantCulture));
+            else if (res is bool) cw.WriteLine(((bool)res) ? "Yes" : "No");
+            else if (res != null) cw.WriteLine(res.ToString());
+            cw.Flush();
+        }
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        private object Calc()
+        {
+            int N = cr;
+
+            return N;
         }
     }
 }
+
