ab0725e1b0b633d8c3bb82b7d9600adf3eb41381
suzuryg <suzuryg@outlook.com>
2021-07-05 22:26:01 +0900

    add: AtCoderソリューションとAnswerプロジェクトを新規作成

---
+++ Answer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Answer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
