76be867acd508e1af915ff5e6ce11d1ff2ad9f3a
suzuryg <suzuryg@outlook.com>
2021-07-06 22:55:35 +0900

    add: ライブラリ呼び出しテスト用クラスを追加

---
+++ Library/Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Suzuryg.Competitive.Library.Test
+{
+    public static class Test
+    {
+        public static string TestMethod()
+        {
+            return "This is a test method.";
+        }
+    }
+}
