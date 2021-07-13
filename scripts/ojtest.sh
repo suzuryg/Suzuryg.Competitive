2b3638943e6fa04cc5759d020d94c30e34b573fa
suzuryg <suzuryg@outlook.com>
2021-07-14 02:03:51 +0900

    change: ojtest ビルドしてからバイナリをojに渡すように変更
    Windows側ではDebugビルド、WSL側ではReleaseビルドを使用する

--- scripts/ojtest.sh
+++ scripts/ojtest.sh
@@ -2,6 +2,8 @@
 
 (
 cd ~/competitive/Answer
-oj t -c "dotnet run" -d "${OLDPWD}"/tests
+mkdir -p ./build
+dotnet build --nologo -c Release -o ./build/
+oj t -c "./build/Answer" -d "${OLDPWD}"/tests
 )
 cp -b --suffix=_$(date +%Y%m%d_%H%M%S) ~/competitive/Answer/Program.cs ~/competitive/Answer/HandMadeMain.cs ~/competitive/Answer/Combined.csx .
