87e595686f37a1f5e4a4c59ea0b4b7c71c5b1d3d
suzuryg <suzuryg@outlook.com>
2021-07-21 23:54:10 +0900

    add: System.Linqのインポートを追加

--- Answer/Program.cs
+++ Answer/Program.cs
@@ -3,6 +3,7 @@ using Kzrnm.Competitive.IO;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Suzuryg.Competitive.Answer
