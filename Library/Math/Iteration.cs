614e0f0be7f5c3802cae062ed9187aabc91f7ca3
suzuryg <suzuryg@outlook.com>
2021-12-20 02:57:42 +0900

    add: 順列の列挙を追加

---
+++ Library/Math/Iteration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Linq;
+
+namespace Suzuryg.Competitive.Library.Math
+{
+    public class Iteration
+    {
+        public static IEnumerable<T[]> Permutation<T>(IEnumerable<T> items) {
+            if (items.Count() == 1) {
+                yield return new T[] { items.First() };
+                yield break;
+            }
+            foreach (var item in items) {
+                var leftside = new T[] { item };
+                var unused = items.Except(leftside);
+                foreach (var rightside in Permutation(unused)) {
+                    yield return leftside.Concat(rightside).ToArray();
+                }
+            }
+        }
+    }
+}
